6b82ad929d8e045a2bfd21678ab94adc9e4b1a76
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-09-01 13:08:32 +0200

    updates profile page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import BarraNavigazioneLinkedIn from "./components/Navbar";
-import ColonnaDestra from "./components/ColonnaDestra";
-import ProfileMainSection from "./components/ProfileMainSection";
-import { Container, Row, Col } from "react-bootstrap";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
 import MyFooter from "./components/MyFooter";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <BrowserRouter>
       <BarraNavigazioneLinkedIn />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div>Home Page</div>
-              <Container>
-                <Row xs={2} className="mt-5">
-                  <Col>
-                    <ProfileMainSection />
-                  </Col>
-                  <Col>
-                    <ColonnaDestra />
-                  </Col>
-                </Row>
-              </Container>
-            </>
-          }
-        />
+        <Route path="/" element={<ProfilePage />} />
         <Route path="/network" element={<div>Network Page</div>} />
         <Route path="/jobs" element={<div>Jobs Page</div>} />
         <Route path="/messaging" element={<div>Messaging Page</div>} />
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,12 +1,18 @@
 import { Col, Container, Row } from "react-bootstrap";
+import ColonnaDestra from "./ColonnaDestra";
+import ProfileMainSection from "./ProfileMainSection";
 
 const ProfilePage = () => {
   return (
     <div style={{ background: "#F4F2EE" }}>
-      <Container>
-        <Row className="d-flex justify-content-center">
-          <Col xs={12} md={8}></Col>
-          <Col></Col>
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col xs={12} md={7}>
+            <ProfileMainSection />
+          </Col>
+          <Col xs={0} md={5}>
+            <ColonnaDestra />
+          </Col>
         </Row>
       </Container>
     </div>
