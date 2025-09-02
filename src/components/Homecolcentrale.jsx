50037ae3bf03c22252ef47752862f923b8537383
AminTabite <Aminepicode13@gmail.com>
2025-09-02 10:27:12 +0200

    inizio home

---
+++ src/components/Homecolcentrale.jsx
@@ -0,0 +1,41 @@
+
+import { Container, Row, Col } from "react-bootstrap";
+
+const Homecolcentrale = ()=>{
+
+return(
+<>
+<Container>
+<Row>
+<Col>
+<div className="d-flex flex-column">    {/*post completo*/}
+<div className="d-flex"> {/*foto profilo nome e lavoro*/}
+<div className="rounded-circle"> <img src="https://dummyimage.com/20x20/000/323cc9" alt="foto profilo " /></div>
+<div><h6>Nome Cognome</h6>
+<p>lavora presso azienda x</p> 
+<p>x giorni fa - <i class="bi bi-globe"></i></p> 
+</div>
+</div>
+
+
+
+
+
+
+
+</div> {/*post completo fine */}
+</Col>
+
+</Row>
+
+
+
+</Container>
+
+</>
+
+)
+
+}
+
+import default Homecolcentrale
