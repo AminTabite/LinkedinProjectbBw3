78808b697e5f513155650105332ed7225536f557
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-02 10:25:14 +0200

    adds redux

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,7 @@ import { BsGrid3X3Gap } from "react-icons/bs";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import clientApi from "../services/api";
+import { useDispatch } from "react-redux";
 
 const BarraNavigazioneLinkedIn = () => {
   const posizioneCorrente = useLocation();
@@ -30,8 +31,11 @@ const BarraNavigazioneLinkedIn = () => {
   const [caricamento, setCaricamento] = useState(true);
   const [mostraDropdown, setMostraDropdown] = useState(false);
   const [suggerimentiCategorie, setSuggerimentiCategorie] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const eAttivo = (percorso) => posizioneCorrente.pathname === percorso;
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const recuperaProfilo = async () => {
@@ -47,6 +51,23 @@ const BarraNavigazioneLinkedIn = () => {
     };
 
     recuperaProfilo();
+    const fetchProfile = async () => {
+      try {
+        const data = await clientApi.ottieniIlMioProfilo();
+
+        console.log(data);
+        dispatch({
+          type: "GET_USER",
+          payload: data,
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
   }, []);
 
   const categoriePopulari = [
