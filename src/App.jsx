616dd8899b813353d21acef0638162a965c218b7
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-03 10:47:50 +0200

    comm2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import BarraNavigazioneLinkedIn from "./components/Navbar";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import MyFooter from "./components/MyFooter";
 import ProfilePage from "./components/ProfilePage";
+import Rete from "./components/Rete";
 
 import HomePage from "./components/HomePage";
 import PaginaLavoro from "./components/JobsPage";
@@ -19,7 +20,7 @@ function App() {
         <BarraNavigazioneLinkedIn />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/network" element={<div>Network Page</div>} />
+          <Route path="/network" element={<Rete />} />
           <Route path="/jobs" element={<PaginaLavoro />} />
           <Route path="/messaging" element={<div>Messaging Page</div>} />
           <Route
---
+++ src/components/Rete.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+import ReteCentrale from "./ReteCentrale";
+
+const Rete = () => {
+  return (
+    <Container style={{ paddingTop: "5em" }}>
+      <Row>
+        <Col xs={0} md={4}>
+          <h1>colonna ds</h1>
+        </Col>
+        <Col xs={12} md={8}>
+          <ReteCentrale />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Rete;
